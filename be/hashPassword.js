ad74492c1d56cf5632517c17c055dd8eaf3577fd
fredrico <frederick@zyrex.com>
2024-11-22 16:45:47 +0700

    docker and environment changes

--- be/hashPassword.js
+++ be/hashPassword.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 
 (async () => {
-  const password = '12345'; // Replace this with the desired password
+  const password = 'Zyr3xuser'; // Replace this with the desired password
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log("Hashed password:", hashedPassword);
 })();
