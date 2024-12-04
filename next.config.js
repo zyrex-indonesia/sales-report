80c9706a96974b3ec267c450eb8d4952f8c4d9ee
fredrico <frederick@zyrex.com>
2024-12-04 11:07:47 +0700

    next config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // Enable static site export
+    // Add other Next.js configurations here if needed
+  };
+  
+  module.exports = nextConfig;
+  
