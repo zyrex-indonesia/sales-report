5eb8fa8f58eb8b7250ef550c34ad051e0effb551
frederick-rico-hartanto <110209251+Frederick-rico-hartanto@users.noreply.github.com>
2024-12-06 14:42:58 +0700

    login fix

--- fe/src/Modules/login/module.tsx
+++ fe/src/Modules/login/module.tsx
@@ -17,7 +17,7 @@ const LoginModule: React.FC<LoginModuleProps> = ({ onLoginSuccess }) => {
     setError(null);
 
     try {
-      const response = await fetch('/api/login', {
+      const response = await fetch('https://api.sales.zyrex.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
